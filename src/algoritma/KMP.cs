ffd9427f40afe7b1a57595afc02aa807ba4476bb
b33rk <brkmanics@gmail.com>
2024-05-25 12:40:40 +0700

    feat: add KMP, LCS, hamming, levenshtein, regex, imagetostring

---
+++ src/algoritma/KMP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class KMP {
+    public static int KMPCompare(string text, string pattern) {
+        int i = 0;
+        int j = 0;
+        int patternLength = pattern.Length;
+        int textLength = text.Length;
+        int[] b = computeBorder(pattern);
+
+        while (i < textLength) {
+            if (pattern[j] == text[i]) {
+                if (j == patternLength - 1) {
+                    return i - patternLength + 1;
+                }
+                i++;
+                j++;
+            } else if (j > 0) {
+                j = b[j - 1];
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+    public static int[] computeBorder(string pattern) {
+        int i = 1;
+        int j = 0;
+        int patternLength = pattern.Length;
+        int[] b = new int[patternLength];
+        b[0] = 0;
+        while (i < patternLength) {
+            if (pattern[j] == pattern[i]) {
+                b[i] = j + 1;
+                i++;
+                j++;
+            } else if (j > 0) {
+                j = b[j - 1];
+            } else {
+                b[i] = 0;
+                i++;
+            }
+        }
+        return b;
+    }
+}
