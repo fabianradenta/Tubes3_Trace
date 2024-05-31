79ed24be2d036a0ec38eba438e1ffd93397e26b8
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-05-31 11:23:27 +0700

    coba pull barok lagi

--- src/BoyerMoore.cs
+++ src/BoyerMoore.cs
@@ -1,27 +1,54 @@
-// public class BoyerMoore {
-//     public static int[] BuildLast(string pattern){
-//         int[] last = new int[128];
-//         for (int i = 0; i < 128; i++) {
-//             last[i] = -1;
-//         }
+using System;
 
-//         for (int i = 0; i < pattern.Length; i++) {
-//             last[pattern[i]] = i;
-//         }
-//         return last;
-//     }
-// }
+public class BoyerMoore {
+    public static int[] BuildLast(string pattern) {
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        for (int i = 0; i < pattern.Length; i++) {
+            last[pattern[i]] = i;
+        }
+        return last;
+    }
 
-// class Program {
+    public static int BmMatch(string text, string pattern) {
+        int[] last = BuildLast(pattern);
+        int n = text.Length;
+        int m = pattern.Length;
+        int i = m - 1;
+
+        if (i > n - 1) {
+            return -1;
+        }
+
+        int j = m - 1;
+        do {
+            if (pattern[j] == text[i]) {
+                if (j == 0) {
+                    return i;
+                }
+                else {
+                    i--;
+                    j--;
+                }
+            }
+            else {
+                int lo = last[text[i]];
+                i += m - Math.Min(j, 1 + lo);
+                j = m - 1; 
+            }
+        } while (i <= n - 1);
+
+        return -1;
+    }
+}
+
+// class Program
+// {
 //     static void Main() {
+//         string text = "a d fexampleafar";
 //         string pattern = "example";
-//         int[] last = BoyerMoore.BuildLast(pattern);
-
-//         Console.WriteLine("Last occurrence array:");
-//         for (int i = 0; i < last.Length; i++) {
-//             if (last[i] != -1) {
-//                 Console.WriteLine($"Character: {(char)i}, Index: {last[i]}");
-//             }
-//         }
+//         Console.WriteLine("Match found at index: " + BoyerMoore.BmMatch(text, pattern));
 //     }
 // }
