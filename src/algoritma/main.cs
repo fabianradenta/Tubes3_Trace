cb5b5460a7543e23a7d582bc1242a5aba852f051
b33rk <brkmanics@gmail.com>
2024-06-09 22:17:44 +0700

    final harusnya

--- src/algoritma/main.cs
+++ src/algoritma/main.cs
@@ -1,85 +1,109 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using ImageProcess;
-using System.Drawing;
-using System.Drawing.Imaging;
 using database;
 
-public class MainProgram{
-    public static List<string> Search_FingerPrint(List<string> ImageAscii, bool isKMP){
+public class MainProgram
+{
+    public static List<string> Search_FingerPrint(List<string> ImageAscii, bool isKMP)
+    {
         string ImageAsciiPartial = GetStringToMatch(ImageAscii);
         Database db = new Database();
         db.connect();
         List<string> allFiles = db.selectPathFromSidikJari();
-        Console.WriteLine(allFiles[0]);
         
         int length = ImageAscii.Count;
-        int width = ImageAscii.ElementAt(0).Length;
+        int width = ImageAscii[0].Length;
         int best_value_fp = 0;
         string result = "";
         bool found = false;
-        foreach (string file in allFiles){
+
+        Parallel.ForEach(allFiles, (file, state) =>
+        {
             List<string> ImageAscii2 = ImageToString.IntToString(ImageToString.ConvertToBinaryImage(file));
             int Hamming_Result = 0;
-            for (int idx = 0; idx < (length < ImageAscii2.Count ? length : ImageAscii2.Count); idx++) {
-                if (isKMP){
-                    if ((KMP.KMPCompare(ImageAscii2.ElementAt(idx), ImageAsciiPartial) != -1)) {
+
+            for (int idx = 0; idx < Math.Min(length, ImageAscii2.Count); idx++)
+            {
+                if (isKMP)
+                {
+                    if (KMP.KMPCompare(ImageAscii2[idx], ImageAsciiPartial) != -1)
+                    {
                         result = file;
                         found = true;
-                        break;
-                    } else {
-                        Hamming_Result += Hamming.hammingDist(ImageAscii2.ElementAt(idx), ImageAscii.ElementAt(idx));
+                        state.Break(); // Exit the loop if match is found
+                    }
+                    else
+                    {
+                        Hamming_Result += Hamming.hammingDist(ImageAscii2[idx], ImageAscii[idx]);
                     }
                 }
-                else {
-                    if ((BoyerMoore.BmMatch(ImageAscii2.ElementAt(idx), ImageAsciiPartial) != -1)) {
+                else
+                {
+                    if (BoyerMoore.BmMatch(ImageAscii2[idx], ImageAsciiPartial) != -1)
+                    {
                         result = file;
                         found = true;
-                        break;
-                    } else {
-                        Hamming_Result += Hamming.hammingDist(ImageAscii2.ElementAt(idx), ImageAscii.ElementAt(idx));
+                        state.Break(); // Exit the loop if match is found
+                    }
+                    else
+                    {
+                        Hamming_Result += Hamming.hammingDist(ImageAscii2[idx], ImageAscii[idx]);
                     }
                 }
             }
+
             Hamming_Result = (length * width - Hamming_Result) * 100 / (length * width);
             if (Hamming_Result > best_value_fp)
             {
                 best_value_fp = Hamming_Result;
                 result = file;
             }
-            if (found) {
-                break;
+
+            if (found)
+            {
+                state.Break(); // Exit the parallel loop if match is found
             }
-        }
-        List<string> res = new List<string>();
-        res.Add(result);
-        if (found){
+        });
+
+        List<string> res = new List<string>
+        {
+            result
+        };
+
+        if (found)
+        {
             res.Add("0");
             res.Add("100");
-        } else {
-            if (best_value_fp < 50) {
+        }
+        else
+        {
+            if (best_value_fp < 55)
+            {
                 res.Add("-1");
-                res.Add(Convert.ToString(best_value_fp));
-            } else {
+                res.Add(best_value_fp.ToString());
+            }
+            else
+            {
                 res.Add("1");
-                res.Add(Convert.ToString(best_value_fp));
+                res.Add(best_value_fp.ToString());
             }
         }
+
         return res;
     }
+
     public static string GetStringToMatch(List<string> imageString)
     {
         int colLength = imageString[0].Length;
-
-        // Determine the row to use
         int selectedRow = imageString.Count / 2;
         int desiredLength = 8;
         int start = Math.Max(0, (colLength - desiredLength) / 2);
-
         int end = Math.Min(colLength, start + desiredLength);
 
-        // Build the result string
         StringBuilder result = new StringBuilder();
         for (int i = start; i < end; i++)
         {
@@ -89,6 +113,8 @@ public class MainProgram{
         return result.ToString();
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
+        // Test code here
     }
 }
